24e5dfe5a1156058b15742f09fecbad33bb02ec9
yuanhewei <yuanhewei@sigo.cn>
2024-04-28 15:24:42 +0800

    feat: board服务代码初始化提交

---
+++ analytics-board/src/test/java/com/itender/analytics/board/AnalyticsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itender.analytics.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
