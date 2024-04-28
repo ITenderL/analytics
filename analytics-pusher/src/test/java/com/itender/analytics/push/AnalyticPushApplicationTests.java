f31537ccc3fe1a2f5342585fbcc9a2002bedf96f
yuanhewei <yuanhewei@sigo.cn>
2024-04-28 11:24:00 +0800

    feat: pusher代码初始化提交

---
+++ analytics-pusher/src/test/java/com/itender/analytics/push/AnalyticPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itender.analytics.push;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
