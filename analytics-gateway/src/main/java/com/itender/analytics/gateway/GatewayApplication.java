cf9cce67761dec2bac281d3bfdf4015a886eaae5
yuanhewei <yuanheweiQAQ@163.com>
2024-09-08 13:31:51 +0800

    feat: 新增网关路由配置

--- analytics-gateway/src/main/java/com/itender/analytics/gateway/GatewayApplication.java
+++ analytics-gateway/src/main/java/com/itender/analytics/gateway/GatewayApplication.java
@@ -1,8 +1,16 @@
 package com.itender.analytics.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+@Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -10,4 +18,19 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    /**
+     * 限流，根据ip进行限流
+     *
+     * @return
+     */
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver() {
+        return exchange -> {
+            String ip = exchange.getRequest().getRemoteAddress().getHostString();
+            //获取远程客户端IP
+            String hostName = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+            log.info("hostName: {}, ip: {}" , hostName, ip);
+            return Mono.just(ip);
+        };
+    }
 }
